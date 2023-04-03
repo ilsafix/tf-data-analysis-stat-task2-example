369dc6f728aaf1ab57d33892c1d02602ca11a47b
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-03 18:20:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,16 +2,36 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import t
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    # Calculate sample mean and sample variance of measurements
+    X_bar = np.mean(x)
+    s_squared = np.var(x, ddof=1)
+    
+    # Calculate sample mean and sample variance of time squared
+    t_squared = 59 ** 2
+    
+    # Calculate acceleration coefficient and its standard error
+    A_bar = X_bar / t_squared
+    s_A = np.sqrt(s_squared / t_squared**2)
+    
+    # Calculate degrees of freedom
+    n = len(x)
+    df = n - 1
+    
+    # Calculate t-value for given trust level and degrees of freedom
+    t_value = t.ppf(1 - alpha / 2, df)
+    
+    # Calculate margin of error
+    margin_error = t_value * s_A / np.sqrt(n)
+    
+    # Calculate confidence interval
+    lower_bound = A_bar - margin_error
+    upper_bound = A_bar + margin_error
+    
+    return (lower_bound, upper_bound)
