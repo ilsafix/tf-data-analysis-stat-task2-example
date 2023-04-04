362fcc86a13c2d3ab46de6345106f1ad91b05fd2
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-04 13:33:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import t
 
 chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    t_squared = 59**2 
+    n = len(x)
+    t_value = t.ppf((1 + p) / 2, n-1)  
+    t_squared = 59**2
+    
     # Calculate acceleration coefficients
     accel_coefficients = 2 * (x + 0.5) / t_squared
-    alpha = 1 - p
 
-    return accel_coefficients.mean() - np.sqrt(np.var(accel_coefficients)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
-           accel_coefficients.mean() - np.sqrt(np.var(accel_coefficients)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
+    # Calculate sample mean and standard deviation
+    sample_mean = np.mean(accel_coefficients)
+    sample_std = np.std(accel_coefficients, ddof=1)
+
+    # Calculate standard error and margin of error
+    standard_error = sample_std / np.sqrt(n)
+    margin_of_error = t_value * standard_error
+
+    # Calculate confidence interval bounds
+    lower_bound = sample_mean - margin_of_error
+    upper_bound = sample_mean + margin_of_error
+    
+    return lower_bound, upper_bound
