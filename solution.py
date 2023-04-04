7a9d1e64451aa042fa62b80fab54bc2458a36854
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-04 13:17:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,10 @@ from scipy.stats import expon
 chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    t_squared = 59**2 
+    # Calculate acceleration coefficients
+    accel_coefficients = 2 * (x + 0.5) / t_squared
     alpha = 1 - p
-    return expon(scale=1).ppf(alpha / 2) / (len(x)*np.min(x)), \
-           expon(scale=1).ppf(1 - alpha / 2) / (len(x)*np.min(x))
+
+    return accel_coefficients.mean() - np.sqrt(np.var(accel_coefficients)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
+           accel_coefficients.mean() - np.sqrt(np.var(accel_coefficients)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
