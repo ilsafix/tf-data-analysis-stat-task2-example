eb3f89acf92f0b282d1ad948a640d8c3fbdb19d2
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-06 12:11:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,28 +1,15 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import norm, erlang
 
 chat_id = 391223586 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    t_value = t.ppf((1 + p) / 2, n-1)  
-    t_squared = 59**2
-    
+    t_squared = 59**2 
     # Calculate acceleration coefficients
-    accel_coefficients = 2 * (x + 0.5) / t_squared
-
-    # Calculate sample mean and standard deviation
-    sample_mean = np.mean(accel_coefficients)
-    sample_std = np.std(accel_coefficients, ddof=1)
-
-    # Calculate standard error and margin of error
-    standard_error = sample_std / np.sqrt(n)
-    margin_of_error = t_value * standard_error
-
-    # Calculate confidence interval bounds
-    lower_bound = sample_mean - margin_of_error
-    upper_bound = sample_mean + margin_of_error
-    
-    return lower_bound, upper_bound
+    accel_coefficients = 2 * x / t_squared
+    alpha = 1 - p
+    s = np.sqrt(np.var(accel_coefficients)) / np.sqrt(len(x))
+    return accel_coefficients.mean() - s * (norm.ppf(1 - alpha / 2) + erlang(a=len(x), loc=0.5, scale=1/len(x)).ppf(p)), \
+           accel_coefficients.mean() - s * (norm.ppf(alpha / 2) - erlang(a=len(x), loc=0.5, scale=1/len(x)).ppf(p))
